417623657c2aa89e07093e665024cdc391328317
Divya2693 <55048794+Divya2693@users.noreply.github.com>
2026-04-30 21:29:15 +0530

    Refactor getBrowserName function with comments

--- Homework-var-let-constLearning.js
+++ Homework-var-let-constLearning.js
@@ -2,7 +2,7 @@
 function getBrowserName(){
     if(browserName === "Chrome"){
         let browserName = "Chrome"; 
-        //let browserName = "Chrome";
+        //var browserName = "Chrome";
     }
     console.log("The browser name inside block is", browserName)
 }
